e5b025f7f93a3f6840476b3bec7bb2b65cfff340
SrinidhiPatil <sripatil1994@gmail.com>
2018-03-04 13:51:20 +0530

    tokenizing exercise

--- tokenizing.py
+++ tokenizing.py
@@ -1,3 +1,5 @@
+#Tokenization is a way to split text into tokens. nltk has several methods to tokenize. Following are word and sentence tokenizers.
+
 from nltk.tokenize import sent_tokenize, word_tokenize
 
 EXAMPLE_TEXT = "Hello Mr. Smith, how are you doing today? The weather is great, and Python is awesome. The sky is pinkish-blue. You shouldn't eat cardboard."
